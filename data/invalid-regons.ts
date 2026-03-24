ead2c5e2ab21f4a78ca1a830a5768a9c35ea63bd
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-24 10:47:29 +0100

    dodanie frazy eksportującej listę numerów REGON spoza baz

---
+++ data/invalid-regons.ts
@@ -0,0 +1,7 @@
+// lista numerów REGON, 
+// które mogą być używane w testach do sprawdzania, jak aplikacja radzi sobie 
+// gdy podamy REGON, który nie istnieje w bazach GUS
+// Typ danych invalidRegons - tablica mogąca zawierać różne wartości,
+export const nonExistingRegons = [
+
+]
