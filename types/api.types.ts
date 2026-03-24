36f880811e29c8f10576942c3b4f66480fa117a0
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-24 10:39:07 +0100

    dodanie pola 'body' do interfejsu

--- types/api.types.ts
+++ types/api.types.ts
@@ -1,3 +1,10 @@
+/* 
+Definicje interfejsów i typów, które będą używane
+do reprezentowania danych zwracanych przez API.
+fraza export - eksportuje interfejs ApiResponse, 
+który może być używany w innych częściach aplikacji.  
+*/
 export interface ApiResponse {
     status: number;
+    body: unknown;
 }
