eb03223c4ff759537a0d3dd1dc159481001d70ec
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-24 10:57:04 +0100

    dodanie kompletu REGONów

--- data/invalid-regons.ts
+++ data/invalid-regons.ts
@@ -1,7 +1,17 @@
-// lista numerów REGON, 
-// które mogą być używane w testach do sprawdzania, jak aplikacja radzi sobie 
-// gdy podamy REGON, który nie istnieje w bazach GUS
+// Lista numerów REGON, 
+// do sprawdzania w testach, jak aplikacja radzi sobie 
+// gdy podamy REGON, który nie istnieje w systemach GUS
 // Typ danych invalidRegons - tablica mogąca zawierać różne wartości,
 export const nonExistingRegons = [
+
+    {
+        regon: '162537906',
+        description: 'REGON poprawny ale bez powiązań z istniejacymi podmiotami'
+    },
+    
+    {
+        regon: '664408741',
+        description: 'REGON poprawny ale bez powiązań z istniejacymi podmiotami'
+    }   
 
 ]
