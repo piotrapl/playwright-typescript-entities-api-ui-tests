8f609a604ea382bbf2ec4702cafd123c1fb5a1b2
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-25 13:32:09 +0100

    definicja metody weryfikującej status odpowiedzi (param.: expectedStatus)

--- assertions/regon.assert.ts
+++ assertions/regon.assert.ts
@@ -1,2 +1,6 @@
 import { expect } from '@playwright/test';
 import { ApiResponse} from '../types/api.types';
+
+export class RegonAssertions {
+    static assertStatusCode(response: ApiResponse, expectedStatus: number) {
+        expect(response.status).toBe(expectedStatus);
