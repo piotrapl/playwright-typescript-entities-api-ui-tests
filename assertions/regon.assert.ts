923ec427680fa605ee9095836d19bf7407646d43
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-25 14:02:17 +0100

    dodanie definicji konstruktora klasy RegonAssertions

--- assertions/regon.assert.ts
+++ assertions/regon.assert.ts
@@ -2,6 +2,13 @@ import { expect } from '@playwright/test';
 import { ApiResponse} from '../types/api.types';
 
 export class RegonAssertions {
+// to jest klasa, która będzie zawierać metody asercji (assertions) do sprawdzania wyników testów.
+// poniżej - 'constructor(' - ten blok kodu definiuje konstruktor klasy, przyjący 2 argumenty:
+//  uiMessage (wiadomość z interfejsu użytkownika) i apiResponse (odpowiedź z API).
+    constructor(
+        private uiMessage: string,
+        private apiResponse: ApiResponse
+    ) {}
     
     assert() {
 
