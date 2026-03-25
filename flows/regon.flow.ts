fb8512220cafee15eb823dd42a6ebd6dd4d3c50d
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-25 16:28:07 +0100

    dodanie 1-szych 2 kroków w def. metody searchRegonAndVerify

--- flows/regon.flow.ts
+++ flows/regon.flow.ts
@@ -4,9 +4,18 @@ import test from '@playwright/test';
 
 export class RegonFlow {
 
-    constructor ( private regonPage: RegonPage,)
+    constructor ( private regonPage: RegonPage) {}
 
-    await test.step('Otwórz stronę wyszukiwania po n-rze Regon', async () => {
-        await this.regonPage.open();
-    });
+    async searchRegonAndVerify(regon: string) {
 
+        await test.step('Otwórz stronę wyszukiwania po REGON-ie', async () => {
+           await this.regonPage.open();
+        });
+        // 'let ApiResponse' to jest zmienna, która będzie przechowywać odpowiedź z API po wysłaniu zapytania o REGON.
+        // "let" to jest słowo kluczowe w JavaScript do deklaracji zmiennej, 
+        //      która może być przypisana do różnych wartości w trakcie działania programu.
+        let apiResponse;
+
+        await test.step(`Wyszukaj REGON i pobierz odpowiedź z backendu`, async () => {
+            apiResponse = await this.regonPage.searchRegon(regon);
+        });
