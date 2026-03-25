4a6a42d827833929134b5fdf5d72c3c50aba03e7
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-25 16:16:21 +0100

    definicja klasy - dodanie def. 2-szego kroku (test.step)

--- flows/regon.flow.ts
+++ flows/regon.flow.ts
@@ -2,3 +2,10 @@ import { RegonPage } from '../pages/regon.page';
 import { RegonAssertions } from '../assertions/regon.assert';
 import test from '@playwright/test';
 
+export class RegonFlow {
+
+
+
+    await test.step('Otwórz stronę wyszukiwania po n-rze Regon', async () => {
+        await this.regonPage.open();
+    });
